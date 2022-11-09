fbe9794881a92ed9ebc68e7caff9281ae1264f29
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-11-10 01:57:38 +0600

    whyMe done

---
+++ src/Component/WhyMe/WhyMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const WhyMe = () => {
+    return (
+        <div className="hero  bg-red-100 ">
+  <div className="hero-content flex-col lg:flex-row py-6 lg:py-0 gap-10">
+  <img src="https://i.ibb.co/ZgnGm0G/20220505-215806-removebg-preview.png" alt="20220505-215806-removebg-preview" className='relative bottom-0' border="0"/>
+    <div className=''>
+      <h1 className="text-6xl font-bold text-red-500">Why Me ? ? ? ? ?</h1>
+      <p className="py-6 text-lg">There is a lot of people in the market who giving you this kind of services in lower cost.But here I try to Provide my client a quality service and a better experience.</p>
+      <button className="btn btn-primary text-white" style={{marginBottom:'20px'}}>Register</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default WhyMe;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slider from '../../Component/Slider/Slider';
+import WhyMe from '../../Component/WhyMe/WhyMe';
 
 const Home = () => {
     return (
         <div className='max-w-screen-xl mx-auto'>
             <Slider></Slider>
+            <WhyMe></WhyMe>
         </div>
     );
 };
